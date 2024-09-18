e68b944573d92de2741a0565ab6035b942ab9174
sentobc13 <sentoberlanga@gmail.com>
2024-09-18 14:54:24 +0200

    products interfsce creada

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { ProductInterface } from '../../interfaces/product-interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-
+public products:Array<ProductInterface> = [
+  {
+    id:1,
+    name:"product 1",
+    price: 10
+  },
+  {
+    id:2,
+    name:"product 2",
+    price: 20
+  },
+  {
+    id:3,
+    name:"product 3",
+    price: 30
+  },
+]
 }
