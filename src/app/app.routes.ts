8138ae5fc46035d0942b758f0289cd02cba5bd08
sentobc13 <sentoberlanga@gmail.com>
2024-09-19 09:16:03 +0200

    characters component creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
 import { HomeComponent } from './components/home/home.component';
+import { CharactersComponent } from './components/characters/characters.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'characters', component: CharactersComponent}
 ];
