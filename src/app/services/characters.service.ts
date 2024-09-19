84ce968f471abc4a15849e4b6a785c814ac1a50c
sentobc13 <sentoberlanga@gmail.com>
2024-09-19 09:29:55 +0200

    characters service creado

---
+++ src/app/services/characters.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharactersService {
+private apiUrl = 'https://rickandmortyapi.com/api/character';
+
+  constructor(private http: HttpClient) {}
+
+  getCharacters(): Observable<any> {
+    return this.http.get(this.apiUrl)
+  }
+}
