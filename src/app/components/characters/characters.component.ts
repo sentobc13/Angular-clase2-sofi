06015b2c244d8df5b6420cbe8a06e214d039111b
sentobc13 <sentoberlanga@gmail.com>
2024-09-19 10:37:47 +0200

    datos api impresos en pantalla

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CharactersInterface } from '../../interfaces/characters-interface';
+import { CharactersService } from '../../services/characters.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-characters',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './characters.component.html',
   styleUrl: './characters.component.css'
 })
-export class CharactersComponent {
 
+export class CharactersComponent implements OnInit{
+  public characters: Array<CharactersInterface> = [];
+
+  constructor(private charactersService: CharactersService) {}
+
+  ngOnInit(): void {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    this.charactersService.getCharacters().subscribe({
+      next: (data) => {
+        this.characters = data.results;
+      },
+      error: (error) => {
+        console.error('Error fetching characters:', error);
+      }
+    })
+  }
 }
+
+// ejemplo:
+
+// export class CharactersComponent implements OnInit{
+// // useEffect(() => {
+
+// // }, [])
+// public fruta = 'manzana'
+
+// ngOnInit(): void {
+//   setTimeout(() => {
+//     this.fruta = 'pera'
+//   }, 2000);
+// }
+// }
---
+++ src/app/interfaces/characters-interface.ts
@@ -0,0 +1,7 @@
+export interface CharactersInterface {
+    id:number;
+    name:string;
+    gender:string;
+    image:string;
+    status:string;
+}
